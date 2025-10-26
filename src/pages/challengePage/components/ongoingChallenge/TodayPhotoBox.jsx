3f94b34c69e4d73a5c28fddd2e16833cc3f13129
Suki <tues1205@naver.com>
2025-10-27 03:30:27 +0900

    ✨ Feat: TodayPhotoBox 컴포넌트

---
+++ src/pages/challengePage/components/ongoingChallenge/TodayPhotoBox.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import s from '../style/TodayPhotoBox.module.scss';
+
+const TodayPhotoBox = ({ name, src }) => {
+  return (
+    <div className={s.todayPhotoBox}>
+      <img src={src} alt="인증된 사진" />
+      <p>{name}</p>
+    </div>
+  );
+};
+
+export default TodayPhotoBox;
