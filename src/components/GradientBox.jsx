69bf13ca0c618bbfec84f78ea81541b218acc0e3
Suki <tues1205@naver.com>
2025-10-28 03:28:46 +0900

    💄 Design: GradientBox 줄바꿈 가능하게

--- src/components/GradientBox.jsx
+++ src/components/GradientBox.jsx
@@ -4,7 +4,7 @@ import s from './style/GradientBox.module.scss';
 const GradientBox = ({ width, height, text, borderRadius }) => {
   return (
     <div className={s.gradientBox} style={{ width, height, borderRadius }}>
-      {text}
+      <p>{text}</p>
     </div>
   );
 };
