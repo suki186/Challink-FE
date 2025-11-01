6c257f964c40f9fc3a11f83fe5357efaea398449
kyooonnnggg <gyoeunjung23@sookmyung.ac.kr>
2025-11-02 01:35:04 +0900

    💄 Design : GradientBox font-size 추가

--- src/components/GradientBox.jsx
+++ src/components/GradientBox.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import s from './style/GradientBox.module.scss';
 
-const GradientBox = ({ width, height, text, borderRadius }) => {
+const GradientBox = ({ width, height, text, borderRadius, square = false, fontSize = '16px' }) => {
+  const boxClassName = `${s.gradientBox} ${square ? s.isSquare : ''}`;
+
   return (
-    <div className={s.gradientBox} style={{ width, height, borderRadius }}>
-      <div className={s.gradientText}>{text}</div>
+    <div className={boxClassName.trim()} style={{ width, height, borderRadius }}>
+      <div className={s.gradientText} style={{ fontSize }}>
+        {text}
+      </div>
     </div>
   );
 };
