a26e8aadf8a14bc88d83a4dda6ab3451a29008fb
Suki <tues1205@naver.com>
2025-11-04 00:58:41 +0900

    💄 Design: 성공률에 따른 상태바

--- src/pages/resultPage/components/ResultItem.jsx
+++ src/pages/resultPage/components/ResultItem.jsx
@@ -3,6 +3,8 @@ import s from './style/ResultItem.module.scss';
 import MY from '../../../assets/images/icons/my_icon.svg';
 
 const ResultItem = ({ isMe = false }) => {
+  const progress = (6 / 7) * 100;
+
   return (
     <div className={s.resultItemContainer}>
       <section className={s.section1}>
@@ -11,7 +13,9 @@ const ResultItem = ({ isMe = false }) => {
       </section>
       <section className={s.section2}>
         <p className={s.count}>22/27</p>
-        <div className={s.statusBar}></div>
+        <div className={s.statusBar}>
+          <div className={s.statusBarFill} style={{ width: `${progress}%` }}></div>
+        </div>
         <p className={s.point}>300,000p</p>
       </section>
     </div>
