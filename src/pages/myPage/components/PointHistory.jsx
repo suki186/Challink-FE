09ddaf57ab7edf82ddb4a373fbf10cfdcca39ff7
Suki <tues1205@naver.com>
2025-11-05 03:04:23 +0900

    🤡 Mock: PointHistory Mock data 반영

--- src/pages/myPage/components/PointHistory.jsx
+++ src/pages/myPage/components/PointHistory.jsx
@@ -2,21 +2,19 @@ import React from 'react';
 import s from './styles/PointHistory.module.scss';
 import PointItem from './PointItem';
 import GradientButton from '../../../components/GradientButton';
+import data from '../datas/pointItemDummy.json';
 
 const PointHistory = () => {
+  const points = data.results;
+
   return (
     <div className={s.pointHistoryContainer}>
       <p className={s.pointTitle}>포인트 내역</p>
       <div className={s.pointItemBox}>
         <div className={s.pointItemBoxWrapper}>
-          <PointItem />
-          <PointItem />
-          <PointItem />
-          <PointItem />
-          <PointItem />
-          <PointItem />
-          <PointItem />
-          <PointItem />
+          {points.map((item) => (
+            <PointItem key={item.history_id} point={item} />
+          ))}
         </div>
       </div>
       <GradientButton width="255px" height="48px" text={`확인`} />
