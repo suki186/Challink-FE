d24b86c2895153bc0355b09269ee4c8dc00fd845
Suki <tues1205@naver.com>
2025-11-07 07:04:40 +0900

    🔧 Setting: api 기본 틀 instance.js 작성

---
+++ src/apis/utils/instance.js
@@ -0,0 +1,54 @@
+import axios from 'axios';
+
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+// ===== 공통 요청 인터셉터
+const onRequestFulfilled = (config) => {
+  // localStorage에서 토큰 가져오기
+  const accessToken = localStorage.getItem('accessToken');
+
+  // 토큰이 있다면 헤더에 추가
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  return config;
+};
+
+const onRequestRejected = (error) => {
+  return Promise.reject(error);
+};
+
+// 기본 인스턴스
+export const defaultInstance = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json; charset=UTF-8',
+  },
+});
+defaultInstance.interceptors.request.use(onRequestFulfilled, onRequestRejected);
+
+// form-data 인스턴스
+export const multiInstance = axios.create({
+  baseURL: BASE_URL,
+});
+multiInstance.interceptors.request.use(onRequestFulfilled, onRequestRejected);
+
+// ===== 공통 응답 인터셉터
+const onResponseFulfilled = (response) => {
+  return response;
+};
+
+const onResponseRejected = (error) => {
+  const { response } = error;
+
+  if (response?.status === 401) {
+    console.error('401 Unauthorized: 토큰이 만료되었거나 유효하지 않습니다.');
+    window.location.href = '/login';
+  }
+
+  return Promise.reject(error);
+};
+
+defaultInstance.interceptors.response.use(onResponseFulfilled, onResponseRejected);
+multiInstance.interceptors.response.use(onResponseFulfilled, onResponseRejected);
