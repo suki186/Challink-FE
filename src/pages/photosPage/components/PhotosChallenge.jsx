cf6cd47d308f47c478f3f7dd48eec4b48757e5f9
Suki <tues1205@naver.com>
2025-11-06 17:17:07 +0900

    💄 Design: 모든사진페이지 이름 필터링 추가

--- src/pages/photosPage/components/PhotosChallenge.jsx
+++ src/pages/photosPage/components/PhotosChallenge.jsx
@@ -3,23 +3,23 @@ import s from './style/PhotosChallenge.module.scss';
 import CHAR from '@assets/images/character.png';
 
 const PhotosChallenge = ({ photoData, onPhotoClick }) => {
-  if (!photoData || !photoData.data || photoData.data.length === 0) {
+  if (!photoData || photoData.length === 0) {
     return (
       <div className={s.noPhotos}>
         <img src={CHAR} alt="캐릭터" width="83px" />
-        <div className={s.firstText}>가장 먼저 도전을 시작해 보세요!</div>
+        <div className={s.firstText}>빨리 도전을 시작해 보세요!</div>
       </div>
     );
   }
 
   return (
     <div className={s.photosContainer}>
-      {photoData.data.map((photo) => (
+      {photoData.map((photo) => (
         <img
-          key={photo.photo_id}
+          key={photo.id}
           className={s.onePhoto}
-          src={photo.image_url}
-          alt={photo.photo_id}
+          src={photo.image}
+          alt={photo.id}
           onClick={() => onPhotoClick(photo)}
         />
       ))}
