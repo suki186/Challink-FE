3676335bc202a334d6905f20f77a1fff676916e5
kyooonnnggg <gyoeunjung23@sookmyung.ac.kr>
2025-11-09 00:48:52 +0900

    ✨ Feat : 회원가입, 로그인 API 추가

--- src/apis/auth/authApi.js
+++ src/apis/auth/authApi.js
@@ -1,18 +1,13 @@
 import { defaultInstance } from '../utils/instance';
 
-// const signupUserApi = async (signupData) => {
-//   const response = await defaultInstance.post('/auth/signup/', signupData);
-
-//   if (response.status === 201) {
-//     console.log('회원가입 성공:', response.data);
-//     return response.data;
-//   }
-
-//   console.warn('요청은 성공했으나 예기치 않은 응답:', response);
-//   throw new Error('서버로부터 예기치 않은 응답을 받았습니다.');
-// };
-
+// 회원가입
 export const signupUserApi = async (data) => {
   const res = await defaultInstance.post('/auth/signup/', data);
+  return res.data;
+};
+
+// 로그인
+export const loginUserApi = async (data) => {
+  const res = await defaultInstance.post('/auth/login/', data);
   return res.data;
 };
