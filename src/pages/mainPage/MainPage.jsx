1689697edb6a76e3a05103ccbf9de40284ac779f
kyooonnnggg <gyoeunjung23@sookmyung.ac.kr>
2025-11-09 01:52:47 +0900

    🔥 Delete : Footer 제거

--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -3,15 +3,13 @@ import Header from './components/Header';
 import MyChallenge from './components/MyChallenge';
 import ChallengeCard from './components/ChallengeCard';
 import AllChallenge from './components/AllChallenge';
-import Footer from '../../components/footer/Footer';
-// import TabBar from '../../components/tabBar/TabBar';
+
 const MainPage = () => {
   return (
     <div>
       <Header />
       <MyChallenge />
       <AllChallenge />
-      <Footer />
     </div>
   );
 };
