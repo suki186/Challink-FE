067b1a7d9dda195402445009a0d9347cd7a879c6
kyooonnnggg <gyoeunjung23@sookmyung.ac.kr>
2025-11-09 02:31:23 +0900

    ✨ Feat : 탭바 생성 버튼 클릭시 이동

--- src/components/tabBar/CreateChallengeBtn.jsx
+++ src/components/tabBar/CreateChallengeBtn.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import s from './style/CreateChallengeBtn.module.scss';
 import PLUS from '../../assets/images/plus_icon.svg';
+import useNavigation from '../../hooks/useNavigation';
 
 // 챌린지 생성 버튼
 const CreateChallengeBtn = ({ className }) => {
+  const { goTo } = useNavigation();
+
   return (
     <div className={`${s.buttonContainer} ${className || ''}`}>
       <img
@@ -14,6 +17,7 @@ const CreateChallengeBtn = ({ className }) => {
           width: '100%',
           maxWidth: 'min(6.11vw, 24px)',
         }}
+        onClick={() => goTo('/challenge/create')}
       />
     </div>
   );
