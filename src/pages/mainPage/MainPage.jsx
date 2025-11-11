79e307f9658c20bb5d333d328c02d66438ea19e8
kyooonnnggg <gyoeunjung23@sookmyung.ac.kr>
2025-11-12 01:37:29 +0900

    ✨ Feat : 메인페이지 검색 기능 api 연동

--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -1,19 +1,43 @@
 import React from 'react';
 import Header from './components/Header';
 import MyChallenge from './components/MyChallenge';
-// import ChallengeCard from './components/ChallengeCard';
 import AllChallenge from './components/AllChallenge';
 import Footer from '../../components/footer/Footer';
+import useModalStore from '../../store/modalStore';
+import ChallengeModal from '../../components/challengeModal/ChallengeModal';
 
 const MainPage = () => {
+  // 1. (무한 루프 방지) 스토어 구독을 '따로' 합니다.
+  const modalData = useModalStore((state) => state.modalData);
+  const closeModal = useModalStore((state) => state.closeModal);
+
+  // 2. 렌더링 함수
+  const renderGlobalModal = () => {
+    if (!modalData) return null;
+
+    const { modalType, modalProps } = modalData;
+
+    if (modalType === 'ChallengeModal') {
+      return <ChallengeModal challengeData={modalProps.challengeData} onClose={closeModal} />;
+    }
+
+    return null;
+  };
+
   return (
-    <div>
-      <Header />
-      <MyChallenge />
-      {/* <ChallengeCard /> */}
-      <AllChallenge />
-      <Footer />
-    </div>
+    // 3. React.Fragment로 감싸기
+    <React.Fragment>
+      {/* 3-1. 페이지 본문 */}
+      <div>
+        <Header />
+        <MyChallenge />
+        <AllChallenge />
+        <Footer />
+      </div>
+
+      {/* 3-2. 모달 렌더링 위치 */}
+      {renderGlobalModal()}
+    </React.Fragment>
   );
 };
 
